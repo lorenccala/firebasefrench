133af089fc1cb45eec44af6991efa953431d250d
Lorenc Çala <lorenc.cala@gmail.com>
2025-05-29 22:08:04 +0000

    make the notification label smaller

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -101,7 +101,7 @@ const ToastTitle = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <ToastPrimitives.Title
     ref={ref}
-    className={cn("text-sm font-semibold", className)}
+    className={cn("text-xs font-semibold", className)}
     {...props}
   />
 ))
@@ -113,7 +113,7 @@ const ToastDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <ToastPrimitives.Description
     ref={ref}
-    className={cn("text-sm opacity-90", className)}
+    className={cn("text-xs opacity-90", className)}
     {...props}
   />
 ))
