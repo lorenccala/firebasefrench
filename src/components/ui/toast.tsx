2f8d2c477f436fd5f046bf5b8dac23ca759b9314
Lorenc Çala <lorenc.cala@gmail.com>
2025-05-29 22:09:15 +0000

    is still to big, make it at least as it is the text.

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -27,7 +27,6 @@ ToastViewport.displayName = ToastPrimitives.Viewport.displayName
 
 const toastVariants = cva(
   "group pointer-events-auto relative flex w-full items-center justify-between space-x-4 overflow-hidden rounded-md border p-6 pr-8 shadow-lg transition-all data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-bottom-full data-[state=open]:sm:slide-in-from-bottom-full",
-  // Adjusted slide-in for default to be from-bottom-full as well
   {
     variants: {
       variant: {
@@ -101,7 +100,7 @@ const ToastTitle = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <ToastPrimitives.Title
     ref={ref}
-    className={cn("text-xs font-semibold", className)}
+    className={cn("text-[10px] font-semibold", className)}
     {...props}
   />
 ))
@@ -113,7 +112,7 @@ const ToastDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <ToastPrimitives.Description
     ref={ref}
-    className={cn("text-xs opacity-90", className)}
+    className={cn("text-[10px] opacity-90", className)}
     {...props}
   />
 ))
